47dab052e3249e47b96cf9e3182a1bcd5095190e
Ilya Savitsky <ipsavitsky234@gmail.com>
2022-04-24 15:11:48 +0300

    deploy script now doesn't download doubles

--- deploy.py
+++ deploy.py
@@ -2,9 +2,16 @@ import wget
 import os
 import subprocess
 
-wget.download(url="https://download.01.org/opencv/openvino_training_extensions/models/human_pose_estimation/checkpoint_iter_370000.pth",
-              out="lhpe")
+if not os.path.exists('lhpe/checkpoint_iter_370000.pth'):
+    wget.download(url="https://download.01.org/opencv/openvino_training_extensions/models/human_pose_estimation/checkpoint_iter_370000.pth",
+                  out="lhpe")
+else:
+    print('checkpoint_iter_370000.pth already exists')
 
-# WARNING: THIS IS A REALLY BAD PRACTICE, REPLACE ASAP
-os.chdir('./pifuhd')
-subprocess.run(['sh', './scripts/download_trained_model.sh'])
+
+if not os.path.exists('pifuhd/checkpoints/pifuhd.pt'):
+    # WARNING: THIS IS A REALLY BAD PRACTICE, REPLACE ASAP
+    os.chdir('./pifuhd')
+    subprocess.run(['sh', './scripts/download_trained_model.sh'])
+else:
+    print('pifuhd.pt already exists')
