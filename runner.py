44ac8b7ef7ca4a8fe3d0b762aa7c882b0ef9f1d6
Maksim Zhelezov <maxzhelezov@gmail.com>
2022-04-27 06:32:10 +0300

    Finish runner

--- runner.py
+++ runner.py
@@ -1,32 +1,41 @@
-import deploy
+import pathlib as pt
+import tempfile
+import argparse
+
+import run_prep
 import run_3ddfa
 import run_pifu
-import shutil
-import os
 
-parent_dir = "./data"
+
+class IOargs():
+    def __init__(self, input, output):
+        self.input_path = input
+        self.output_path = output
+
+
+def run(input, output, mode):
+    data = pt.Path(input)
+    result = pt.Path(output)
+    with tempfile.TemporaryDirectory() as prep:
+        print('Running preprocessing')
+        run_prep.main(IOargs(data, prep))
+
+        if mode == "body":
+            print('Running pifu')
+            run_pifu.main(IOargs(prep, result))
+        else:
+            print('Running 3ddfa')
+            run_3ddfa.main(IOargs(prep, result))
+
+
+def main(args):
+    run(args.input_path, args.output_path, args.mode)
+
 
 if __name__ == '__main__':
-    prep_path = os.path.join(parent_dir, "preps")
-    os.makedirs(prep_path, 0o777)
-    print("Directory '%s' created" % prep_path)
-
-    ddfa_path = os.path.join(parent_dir, "3ddfa")
-    os.makedirs(ddfa_path, 0o777)
-    print("Directory '%s' created" % ddfa_path)
-
-    pifu_path = os.path.join(parent_dir, "3ddfa")
-    os.makedirs(pifu_path, 0o777)
-    print("Directory '%s' created" % pifu_path)
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_path', type=str, default=prep_path)
-    parser.add_argument('-o', '--out_path', type=str, default=pifu_path)
-    run_pifu.main(parser.parse_args())
-
-    parser = argparse.ArgumentParser(description='The demo of still image of 3DDFA_V2')
-    parser.add_argument('-i', '--input_path', type=str, default=pifu_path)
-    parser.add_argument('-o', '--output_path', type=str, default=ddfa_path)
-    run_3ddfa.main(parser.parse_args())
-
-    shutil.rmtree(parent_dir)
+    parser = argparse.ArgumentParser(description='Runner')
+    parser.add_argument('-i', '--input_path', type=str, default='./data')
+    parser.add_argument('-o', '--output_path', type=str, default='./result')
+    parser.add_argument('-m', '--mode', type=str, choices=['face', 'body'], default='body')
+    args = parser.parse_args()
+    main(args)
